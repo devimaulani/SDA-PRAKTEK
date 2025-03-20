9dc6f259f0d517b5800ab8e75a071643a92866ea
devimaulani <devimaulani2204@gmail.com>
2025-03-20 23:59:13 +0700

    Delete TUGAS 6/ADT QUEUE/boolean.h

--- TUGAS 6/ADT QUEUE/boolean.h
+++
@@ -1,9 +0,0 @@
-/* File        : boolean.h */
-/* Deskripsi   : header file untuk menangani type boolean */
-
-#ifndef boolean_H
-#define boolean_H
-#define true 1
-#define false 0
-#define boolean unsigned char
-#endif

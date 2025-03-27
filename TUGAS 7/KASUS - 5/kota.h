6ae0325b6d6ad3d966c09a42151dd4533a59a303
devimaulani <devimaulani2204@gmail.com>
2025-03-27 23:29:23 +0700

    Update kota.h

--- TUGAS 7/KASUS - 5/kota.h
+++ TUGAS 7/KASUS - 5/kota.h
@@ -1,3 +1,13 @@
+/*Author: Devi Maulnai 
+NIM: 241524007
+Kelas: 1A
+Prodi: Sarjana Terapan Teknik Informatika
+Jurusan: Teknik Komputer dan Informatika
+Politeknik Negeri Bandung
+*/
+
+
+
 #ifndef KOTA_H
 #define KOTA_H
 

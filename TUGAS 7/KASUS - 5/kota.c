42603baecc643eb36056db764e21e4559227d68d
devimaulani <devimaulani2204@gmail.com>
2025-03-28 21:38:11 +0700

    Add files via upload

--- TUGAS 7/KASUS - 5/kota.c
+++ TUGAS 7/KASUS - 5/kota.c
@@ -6,174 +6,97 @@ Jurusan: Teknik Komputer dan Informatika
 Politeknik Negeri Bandung
 */
 
-
-
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include "kota.h"
 
-// Inisialisasi daftar kota
-void inisialisasiDaftarKota(DaftarKota *daftarKota) {
-	int i = 0;
-	
-    daftarKota->jumlahKota = 0;
-    for ( i < MAX_KOTA; i++; ) {
-        daftarKota->data[i].p = NULL;
-        
+// Tambah kota baru (tanpa nama di dalamnya)
+void TambahKota(Kota A[], int *jumlahKota, char *kota) {
+    if (*jumlahKota >= MAX_KOTA) {
+        printf("Kapasitas kota penuh!\n");
+        return;
     }
-}
 
-// Tambah kota baru
-boolean tambahKota(DaftarKota *daftarKota, char* namaKota) {
-    if (daftarKota->jumlahKota >= MAX_KOTA) {
-        printf("Jumlah kota sudah mencapai batas maksimum!\n");
-        return false;
-    }
-    
     // Cek apakah kota sudah ada
-    int i = 0;
-    for ( i < daftarKota->jumlahKota; i++;) {
-        if (strcmp(daftarKota->data[i].kt, namaKota) == 0) {
+	int i;
+	for (i = 0; i < *jumlahKota; i++) {  
+        if (strcmp(A[i].kt, kota) == 0) {
             printf("Kota sudah ada!\n");
-            return false;
-        }
-    }
-    
-    // Tambah kota baru
-    strcpy(daftarKota->data[daftarKota->jumlahKota].kt, namaKota);
-    daftarKota->data[daftarKota->jumlahKota].p = NULL;
-    daftarKota->jumlahKota++;
-    return true;
-}
-
-// Tambah nama ke kota tertentu
-boolean tambahNamaKeKota(DaftarKota *daftarKota, char* namaKota, infotype nama) {
-	int i = 0;
-    for ( i < daftarKota->jumlahKota; i++;) {
-        if (strcmp(daftarKota->data[i].kt, namaKota) == 0) {
-            // Buat node baru
-            address nodeBaruNama;
-            Create_Node(&nodeBaruNama);
-            Isi_Node(&nodeBaruNama, nama);
-            
-            // Tambahkan ke linked list nama kota
-            Ins_Akhir(&daftarKota->data[i].p, nodeBaruNama);
-            return true;
-        }
-    }
-    
-    printf("Kota tidak ditemukan!\n");
-    return false;
-}
-
-// Tampilkan nama-nama dalam satu kota
-void tampilkanNamaKota(DaftarKota *daftarKota, char* namaKota) {
-	int i = 0;
-    for ( i < daftarKota->jumlahKota; i++;) {
-        if (strcmp(daftarKota->data[i].kt, namaKota) == 0) {
-            printf("Daftar nama di kota %s:\n", namaKota);
-            Tampil_List(daftarKota->data[i].p);
             return;
         }
     }
-    printf("Kota tidak ditemukan!\n");
+
+    strcpy(A[*jumlahKota].kt, kota);
+    A[*jumlahKota].p = NULL;  // Linked list awalnya kosong
+    (*jumlahKota)++;
+    printf("Kota %s berhasil ditambahkan.\n", kota);
 }
 
-// Hapus semua nama dalam satu kota
-void hapusSemuaNamaKota(DaftarKota *daftarKota, char* namaKota) {
-	int i = 0;
-    for ( i < daftarKota->jumlahKota; i++;) {
-        if (strcmp(daftarKota->data[i].kt, namaKota) == 0) {
-            // Hapus seluruh linked list nama
-            while (daftarKota->data[i].p != NULL) {
-                infotype namaDihapus;
-                Del_Awal(&daftarKota->data[i].p, &namaDihapus);
+// Tambah nama ke dalam kota tertentu
+void TambahNama(Kota A[], int jumlahKota, char *nama, char *kota) {
+	int i;
+    for ( i=0; i < jumlahKota; i++) {
+        if (strcmp(A[i].kt, kota) == 0) { 
+            address PNew;
+            Create_Node(&PNew);
+            Isi_Node(&PNew, nama);
+            Ins_Akhir(&A[i].p, PNew);
+            printf("Nama %s berhasil ditambahkan ke kota %s.\n", nama, kota);
+            return;
+        }
+    }
+    printf("Kota %s tidak ditemukan.\n", kota);
+}
+
+// Hapus kota beserta semua nama di dalamnya
+void HapusKota(Kota A[], int *jumlahKota, char *kota) {
+	int i ;
+    for ( i = 0; i < *jumlahKota; i++) {
+        if (strcmp(A[i].kt, kota) == 0) {
+            address current = A[i].p;
+            while (current != NULL) {
+                address temp = current;
+                current = current->next;
+                free(temp);
             }
-            printf("Semua nama di kota %s telah dihapus\n", namaKota);
+
+            // Geser elemen array setelah kota yang dihapus
+            int j ;
+            for ( j = i; j < *jumlahKota - 1; j++) {
+                A[j] = A[j + 1];
+            }
+
+            (*jumlahKota)--;
+            printf("Kota %s berhasil dihapus.\n", kota);
             return;
         }
     }
-    printf("Kota tidak ditemukan!\n");
+    printf("Kota %s tidak ditemukan.\n", kota);
 }
 
-// Hitung total nama dalam satu kota
-int hitungNamaKota(DaftarKota *daftarKota, char* namaKota) {
-	int i = 0; 
-    for (i < daftarKota->jumlahKota; i++;) {
-        if (strcmp(daftarKota->data[i].kt, namaKota) == 0) {
-            return NbElmt(daftarKota->data[i].p);
+// Menampilkan semua data kota beserta nama-nama di dalamnya
+void Tampilkan(Kota A[], int jumlahKota) {
+    if (jumlahKota == 0) {
+        printf("Tidak ada data kota.\n");
+        return;
+    }
+	int i;
+    for (i = 0; i < jumlahKota; i++) {
+        printf("Kota: %s\n", A[i].kt);
+        Tampil_List(A[i].p);
+    }
+}
+
+// Menampilkan data dari kota tertentu
+void TampilkanPerKota(Kota A[], int jumlahKota, char *kota) {
+	int i;
+	for ( i = 0; i < jumlahKota; i++) {
+        if (strcmp(A[i].kt, kota) == 0) {
+            printf("Kota: %s\n", A[i].kt);
+            Tampil_List(A[i].p);
+            return;
         }
     }
-    return 0;
+    printf("Kota %s tidak ditemukan.\n", kota);
 }
 
-// Tampilkan informasi total
-void tampilkanInfoTotal(DaftarKota *daftarKota) {
-    int totalNama = 0;
-    
-    printf("Informasi Total:\n");
-    printf("Jumlah Kota: %d\n", daftarKota->jumlahKota);
-    
-    int i = 0;
-    for ( i < daftarKota->jumlahKota; i++;) {
-        int jumlahNamaKota = NbElmt(daftarKota->data[i].p);
-        totalNama += jumlahNamaKota;
-        printf("Kota %s: %d nama\n", daftarKota->data[i].kt, jumlahNamaKota);
-    }
-    
-    printf("Total Nama: %d\n", totalNama);
-}
-
-// Menu utama
-void menuUtama(DaftarKota *daftarKota) {
-    int pilihan;
-    char namaKota[PANJANG_NAMA];
-    infotype nama;
-
-    do {
-        printf("\n--- PROGRAM MANAJEMEN DATA KOTA ---\n");
-        printf("1. Tambah Kota\n");
-        printf("2. Tambah Nama ke Kota\n");
-        printf("3. Tampilkan Nama Kota\n");
-        printf("4. Hapus Semua Nama di Kota\n");
-        printf("5. Tampilkan Info Total\n");
-        printf("6. Keluar\n");
-        printf("Pilih menu (1-6): ");
-        scanf("%d", &pilihan);
-
-        switch(pilihan) {
-            case 1:
-                printf("Masukkan nama kota: ");
-                scanf("%s", namaKota);
-                tambahKota(daftarKota, namaKota);
-                break;
-            case 2:
-                printf("Masukkan nama kota: ");
-                scanf("%s", namaKota);
-                printf("Masukkan nama (dalam bilangan): ");
-                scanf("%d", &nama);
-                tambahNamaKeKota(daftarKota, namaKota, nama);
-                break;
-            case 3:
-                printf("Masukkan nama kota: ");
-                scanf("%s", namaKota);
-                tampilkanNamaKota(daftarKota, namaKota);
-                break;
-            case 4:
-                printf("Masukkan nama kota: ");
-                scanf("%s", namaKota);
-                hapusSemuaNamaKota(daftarKota, namaKota);
-                break;
-            case 5:
-                tampilkanInfoTotal(daftarKota);
-                break;
-            case 6:
-                printf("Terima kasih!\n");
-                break;
-            default:
-                printf("Pilihan tidak valid!\n");
-        }
-    } while (pilihan != 6);
-}
--- TUGAS 7/KASUS - 5/kota.h
+++ TUGAS 7/KASUS - 5/kota.h
@@ -6,38 +6,24 @@ Jurusan: Teknik Komputer dan Informatika
 Politeknik Negeri Bandung
 */
 
-
 #ifndef KOTA_H
 #define KOTA_H
 
 #include "linked.h"
-#include "boolean.h"
 
-#define MAX_KOTA 5
-#define PANJANG_NAMA 50
+#define MAX_KOTA 100
 
-// Struktur untuk elemen array kota
 typedef struct {
-    char kt[PANJANG_NAMA];  // nama kota
-    address p;              // pointer ke daftar nama
-} ElemenKota;
+    char kt[50];  // Nama kota
+    address p;    // Pointer ke linked list nama-nama di kota ini
+} Kota;
 
-// Tipe data untuk kontainer kota
-typedef struct {
-    ElemenKota data[MAX_KOTA];
-    int jumlahKota;
-} DaftarKota;
+void TambahKota(Kota A[], int *jumlahKota, char *kota);
+void TambahNama(Kota A[], int jumlahKota, char *nama, char *kota);
+void HapusKota(Kota A[], int *jumlahKota, char *kota);
+void Tampilkan(Kota A[], int jumlahKota);
+void TampilkanPerKota(Kota A[], int jumlahKota, char *kota);
 
-// Prototype fungsi
-void inisialisasiDaftarKota(DaftarKota *daftarKota);
-boolean tambahKota(DaftarKota *daftarKota, char* namaKota);
-boolean tambahNamaKeKota(DaftarKota *daftarKota, char* namaKota, infotype nama);
-void tampilkanNamaKota(DaftarKota *daftarKota, char* namaKota);
-void hapusSemuaNamaKota(DaftarKota *daftarKota, char* namaKota);
-int hitungNamaKota(DaftarKota *daftarKota, char* namaKota);
-void tampilkanInfoTotal(DaftarKota *daftarKota);
+#endif
 
-// Fungsi tambahan untuk menu
-void menuUtama(DaftarKota *daftarKota);
 
-#endif // KOTA_NAMA_H

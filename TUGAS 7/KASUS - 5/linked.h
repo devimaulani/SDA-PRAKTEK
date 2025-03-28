1e6453996f5b81bd7144f119f128a5361f068273
devimaulani <devimaulani2204@gmail.com>
2025-03-28 21:32:20 +0700

    Update linked.h

--- TUGAS 7/KASUS - 5/linked.h
+++ TUGAS 7/KASUS - 5/linked.h
@@ -1,28 +1,25 @@
-
-/* Author: Devi Maulnai 
-   NIM: 241524007
-   Kelas: 1A
-   Prodi: Sarjana Terapan Teknik Informatika
-   Jurusan: Teknik Komputer dan Informatika
-   Politeknik Negeri Bandung
+/*Author: Devi Maulnai 
+NIM: 241524007
+Kelas: 1A
+Prodi: Sarjana Terapan Teknik Informatika
+Jurusan: Teknik Komputer dan Informatika
+Politeknik Negeri Bandung
 */
 
 #ifndef LINKED_H
 #define LINKED_H
-
 #include <stdio.h>
-#include <stdlib.h>  // Digunakan untuk malloc
-#include <string.h>  // Untuk operasi string seperti strcmp, strcpy
+#include <stdlib.h>
+#include <string.h> 
 #include "boolean.h"
 
-typedef char infotype[30];  // Menggunakan string sebagai tipe data
+typedef char infotype[50]; // Ubah menjadi string
 typedef struct tElmtList *address;
 typedef struct tElmtList {
     infotype info;
     address next;
 } ElmtList;
 
-// Prototipe fungsi dasar Linked List
 boolean isEmpty(address p);
 void Create_Node(address *p);
 void Isi_Node(address *p, infotype nilai);
@@ -37,13 +34,5 @@ void Del_After(address *pBef, infotype *X);
 void DeAlokasi(address *p);
 int NbElmt(address p);
 
-// Fungsi tambahan untuk memodifikasi elemen dalam linked list
-void modify_first(address *head, infotype new_data);
-void modify_last(address *head, infotype new_data);
-void modify_middle(address *head, infotype new_data, int position);
-
 #endif
-
-
-
 

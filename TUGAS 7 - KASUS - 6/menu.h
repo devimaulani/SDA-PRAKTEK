6a4c81ba7f677fd597dc93a50a4826553046ddf6
devimaulani <devimaulani2204@gmail.com>
2025-04-07 20:13:49 +0700

    Add files via upload

---
+++ TUGAS 7 - KASUS - 6/menu.h
@@ -0,0 +1,16 @@
+/*Author: Devi Maulnai 
+NIM: 241524007
+Kelas: 1A
+Prodi: Sarjana Terapan Teknik Informatika
+Jurusan: Teknik Komputer dan Informatika
+Politeknik Negeri Bandung
+*/
+
+#ifndef MENU_H
+#define MENU_H
+
+#include "kota.h"
+
+void tampilkanMenu(KotaAddress *daftarKota);
+
+#endif
